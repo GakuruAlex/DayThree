975a66eaeb90d92d2ad791cf51fe191efa0ab9c0
alexgicimo <gakurualexgicimo@gmail.com>
2024-07-31 11:22:39 +0000

    Is even function

---
+++ OddOrEven/is_even.py
@@ -0,0 +1,10 @@
+def is_even(number: int) -> bool:
+    """_Function to check if number is odd or even_
+
+    Args:
+        number (int): _Number to check_
+
+    Returns:
+        bool: _True if number is even otherwise False_
+    """
+    return number % 2 == 0
